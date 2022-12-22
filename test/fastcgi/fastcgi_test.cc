b038a9d72dedc212e7b55c3dbc218e6e03923226
Zehao Lou <zehaolou@outlook.com>
2022-12-22 17:57:24 +0800

    [Typo] Fix typo

--- test/fastcgi/fastcgi_test.cc
+++ test/fastcgi/fastcgi_test.cc
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) hepangda
  *
- * This source cod is licensed under the Apache license found in the
+ * This source code is licensed under the Apache license found in the
  * LICENSE file in the root directory of this source tree.
  */
 #include "source/fastcgi/fastcgi.h"
